50c6f2e56b14475fce162fca01bf12dcf2df0278
Onkar k <onkarok0273.1@gmail.com>
2026-02-26 12:11:44 +0530

    feat: add authorization middleware

--- src/category/category-route.ts
+++ src/category/category-route.ts
@@ -5,11 +5,15 @@ import { CategoryService } from './category-service';
 import logger from '../config/logger';
 import { asyncWrapper } from '../common/utils/wrapper';
 import authenticate from '../common/middlewares/authenticate';
+import { canAccess } from '../common/middlewares/canAccess';
+import { Roles } from '../common/constants';
 
 const router = express.Router();
 const categoryService = new CategoryService();
 const categoeryController = new CategoeryController(categoryService, logger);
 
-router.route('/').post(authenticate, categoryValidator, asyncWrapper(categoeryController.create));
+router
+  .route('/')
+  .post(authenticate, canAccess([Roles.ADMIN]), categoryValidator, asyncWrapper(categoeryController.create));
 
 export default router;
--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -1,3 +1,13 @@
+import type { Request } from 'express';
+
 export type AuthCookie = {
   accessToken: string;
 };
+
+export interface AuthRequest extends Request {
+  auth: {
+    sub: string;
+    role: string;
+    id?: string;
+  };
+}
